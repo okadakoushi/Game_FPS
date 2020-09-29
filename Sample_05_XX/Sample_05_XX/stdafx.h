40b57f8ece773486760abc00e6cd6b662a7bd63e
K_okada <kbc19b06@stu.kawahara.ac.jp>
2020-09-29 15:12:20 +0900

    Game gitignore added

---
+++ Sample_05_XX/Sample_05_XX/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+const int NUM_DIRECTIONAL_LIGHT = 4;	//ディレクションライトの数。
+
+#include "MiniEngine.h"
+
+/// <summary>
+/// ディレクションライト。
+/// </summary>
+struct DirectionalLight {
+	Vector3 direction;	//ライトの方向。
+	float pad0;			//パディング。
+	Vector4 color;		//ライトのカラー。
+};
+/// <summary>
+/// ライト構造体。
+/// </summary>
+struct Light {
+	DirectionalLight directionalLight[NUM_DIRECTIONAL_LIGHT];	//ディレクションライト。
+	Vector3 eyePos;					//カメラの位置。
+	float specPow;					//スペキュラの絞り。
+	Vector3 ambinetLight;			//環境光。
+};
